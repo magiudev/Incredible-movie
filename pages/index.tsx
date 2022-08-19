db8b9750dbb3dd2f29100e0c3205565201c5a813
Miguel Angel Echeverri Quiroz <magiudev@gmail.com>
2022-08-19 11:04:16 +0200

    Insert Navbar and And Filter in the Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
+import Navbar from '../components/organisms/navbar';
+import Filter from '../components/organisms/filter';
 
 const Home: NextPage = () => {
-  return (<h1>Hello world!</h1>);
+  return (
+    <>
+      <Navbar />
+      <Filter />
+    </>
+  );
 };
 
 export default Home;
