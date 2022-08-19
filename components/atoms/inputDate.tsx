ae99cf774a4ff602de2048d0e139e5ed9ed62ba5
Miguel Angel Echeverri Quiroz <magiudev@gmail.com>
2022-08-19 10:37:29 +0200

    Add the InputDate atom

---
+++ components/atoms/inputDate.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InputDate = () => {
+  return (
+    <input className="focus:outline-none bg-gray-200 p-1 m-1 rounded-sm" type="date" />
+  );
+};
+
+export default InputDate;
