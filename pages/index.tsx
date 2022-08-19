ae2ce4ece61bdc1e27e9a1cecea96b3ee4f0f38e
Miguel Angel Echeverri Quiroz <magiudev@gmail.com>
2022-08-19 14:46:13 +0200

    Get top 8 movies using getServerSideProps in Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Navbar from '../components/organisms/navbar';
 import Filter from '../components/organisms/filter';
+import Carrousel from '../components/organisms/carrousel';
+import moviesApiCalls from '../service/moviesApiCalls';
+import { recordType } from '../database/data';
 
-const Home: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await moviesApiCalls.getTop8();
+  return {
+    props: data
+  };
+};
+
+const Home: NextPage<{data: recordType[]}> = ({data}) => {
   return (
     <>
       <Navbar />
       <Filter />
+      <Carrousel cards={data} />
+
     </>
   );
 };
